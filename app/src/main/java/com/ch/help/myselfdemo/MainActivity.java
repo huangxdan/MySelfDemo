94faf12c7d771c183dbb3eb8307b10a8186d1432
chaolele <274511193@qq.com>
2016-06-14 12:31:32 +0800

    chaolele

--- app/src/main/java/com/ch/help/myselfdemo/MainActivity.java
+++ app/src/main/java/com/ch/help/myselfdemo/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //我自己的demo
+        //我自己的demo chaolele
     }
 }
